80c8022e763c515b46df781d10059113d9df9ba7
Snipsx17 <ing.uberthhernandez@gmail.com>
2023-12-12 22:29:41 -0500

    component Button

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Button from "./components/Button";
 import NotFoundPage from "./pages/404/NotFoundPage";
 import LoginPage from "./pages/auth/loginPage/LoginPage";
 import { Routes, Route, Navigate } from "react-router-dom";
@@ -5,9 +6,8 @@ import { Routes, Route, Navigate } from "react-router-dom";
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Button>login</Button>} />
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/" element={<div>Index</div>} />
-      <Route path="/" element={<div>Index</div>} />
       <Route path="/404" element={<NotFoundPage />} />
       <Route path="*" element={<Navigate to="/404" />} />
     </Routes>
---
+++ src/components/Button.js
@@ -0,0 +1,21 @@
+import { styled } from "styled-components";
+
+const Button = styled.button`
+  background-color: ${(props) =>
+    props.$variant === "primary" ? "#3dd2ba" : "#fff"};
+  color: ${(props) => (props.$variant === "primary" ? "#000" : "#3dd2ba")};
+  border: ${(props) =>
+    props.$variant === "primary" ? "none" : "2px solid #3dd2ba"};
+  padding: 1rem 2rem;
+  border-radius: 999px;
+  text-transform: uppercase;
+
+  &:hover {
+    cursor: pointer;
+    opacity: ${(props) => (props.$variant === "primary" ? "0.8" : "1")};
+    background-color: ${(props) =>
+      props.$variant === "primary" ? "#3dd2ba" : "rgba(199, 255, 246, 0.5)"};
+  }
+`;
+
+export default Button;

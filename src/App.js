67f3a841710007e02ec5a57d5fc77b1e3a48a688
Snipsx17 <ing.uberthhernandez@gmail.com>
2023-12-20 16:04:06 -0500

    Ad routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import Button from "./components/Button";
-import NotFoundPage from "./pages/404/NotFoundPage";
-import LoginPage from "./pages/auth/loginPage/LoginPage";
-import { Routes, Route, Navigate } from "react-router-dom";
+import Layout from './components/layout/Layout';
+import NotFoundPage from './pages/404/NotFoundPage';
+import AdvertsPage from './pages/AdvertsPage/AdvertsPage';
+import AuthRequired from './pages/auth/AuthRequired';
+import LoginPage from './pages/auth/loginPage/LoginPage';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Button>login</Button>} />
+      <Route path="/adverts" element={<Layout />}>
+        <Route
+          index
+          element={
+            <AuthRequired>
+              <AdvertsPage />
+            </AuthRequired>
+          }
+        />
+        <Route path="new" element={<div>New advert</div>} />
+        <Route path=":id" element={<div>Advert detail</div>} />
+      </Route>
+      <Route path="/" element={<Navigate to="/adverts" />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/404" element={<NotFoundPage />} />
       <Route path="*" element={<Navigate to="/404" />} />

6987a2ffd8e1f76b49087ed26369079faf409d9f
Snipsx17 <ing.uberthhernandez@gmail.com>
2023-12-22 21:33:37 -0500

    Add layout to 404 page

--- src/pages/404/NotFoundPage.js
+++ src/pages/404/NotFoundPage.js
@@ -1,13 +1,21 @@
-import { Link } from "react-router-dom";
-import "./NotFoundPage.css";
+import { Link } from 'react-router-dom';
+import './NotFoundPage.css';
+import Header from '../../components/layout/Header';
+import Footer from '../../components/layout/Footer';
 
 const NotFoundPage = () => {
   return (
-    <div className="container-center not-found">
-      <h2>Page not found</h2>
-      <img src="page-not-found.jpeg" alt="Not-found-page" />
-      <Link to="/">Go To Home...</Link>
-    </div>
+    <>
+      <Header />
+      <main className="container">
+        <div className="container-center not-found">
+          <h2>Page not found</h2>
+          <img src="page-not-found.jpeg" alt="Not-found-page" />
+          <Link to="/">Go To Home...</Link>
+        </div>
+      </main>
+      <Footer />
+    </>
   );
 };
 

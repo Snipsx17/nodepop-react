34b9dd97d6ad7d439e103a41de0c1541423c6dfe
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-02-20 10:23:35 -0500

    Change pictures path

--- src/pages/404/NotFoundPage.js
+++ src/pages/404/NotFoundPage.js
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import './NotFoundPage.css';
 import Header from '../../components/layout/Header';
 import Footer from '../../components/layout/Footer';
+const pageNotFound = require('../../assets/page-not-found.jpeg');
 
 const NotFoundPage = () => {
   return (
@@ -10,7 +11,7 @@ const NotFoundPage = () => {
       <main className="container">
         <div className="container-center not-found">
           <h2>Page not found</h2>
-          <img src="page-not-found.jpeg" alt="Not-found-page" />
+          <img src={pageNotFound} alt="not page found" />
           <Link to="/">Go To Home...</Link>
         </div>
       </main>
